79a9a61c31dfbe5674aeef3509cd033c6d77d2cf
mkfaraz@umich.edu <mkfaraz@umich.edu>
2024-03-18 13:08:45 -0400

    update

--- Lab15Functions.cpp
+++ Lab15Functions.cpp
@@ -1,28 +1,36 @@
+#include <iostream>
+
 /*******************************************************************
 * getInfo *
 * Gets and validates lottery info from the user and places it in   *
 * reference parameters referencing variables in the main function. *
 *******************************************************************/
-void getInfo(int & pickFrom, int & numPicks){
+void getInfo(int & pickFrom, int & numPicks);
+double computeWays(int n, int k);
+double factorial(int n);
 
+int main() {
+    int pickFrom, numPicks;
+    getInfo(pickFrom, numPicks);
+    double ways = computeWays(pickFrom, numPicks);
+    std::cout << "Number of possible combinations: " << ways << std::endl;
+    return 0;
 }
-
-/******************************************************************
-* computeWays                                                     *
-* Computes and returns the number of different possible sets      *
-* of k numbers that can be chosen from a set of n numbers.        *
-* The formula for this is     k!(n- k)!                           *
-*                             --------                            *
-*                                 n!                              *
-*******************************************************************/
-// Note that the computation is done in a way that does not require
-// multiplying two factorials together. This is done to prevent any
-// intermediate result becoming so large that it causes overflow.
+void getInfo(int &pickFrom, int &numPicks){
+    std::cout << "Enter the total number of numbers to pick from: ";
+    std::cin >> pickFrom;
+    std::cout << "Enter the number of numbers to pick: ";
+    std::cin >> numPicks;
+}
 double computeWays(int n, int k){
-    
+    double ways = factorial(n) / factorial(k) * factorial(n-k);
+    return ways;
 }
 
-// This function computes factorials recursively. It is called by computeWays.
-double factorial(int n){
-
+double factorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
 }
+
